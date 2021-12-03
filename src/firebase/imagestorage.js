9648b2529cbfed05c0d2ebcbade9e2da8bbdca6f
anya-ji <anyaj0109@gmail.com>
2021-12-03 16:48:07 -0500

    Anya/refactor (#11)
    * filter UI
    
    todo:
    1. task drop down
    2. remove
    3. filter
    
    * remove
    
    * filtering
    
    * actually working filter
    
    * remove unused file
    
    * added button
    
    * download single image
    
    * limit initial download and show defaul images again when no filter
    
    * delete commented code
    
    * UI
    
    * add task and assign task to user
    
    * remove comments
    
    * add admin
    
    * auth
    
    * gitignore
    
    * delete admin stuff, merge
    
    * refactor pipeline
    
    * create users
    
    * pipeline done, console needs debug
    
    * omg async finally worked im crying
    
    * all async fixed
    
    Co-authored-by: Ruchitha Rajaghatta <ruchitha.sr@gmail.com>

--- src/firebase/imagestorage.js
+++ src/firebase/imagestorage.js
@@ -11,75 +11,52 @@ import {
 } from "firebase/firestore";
 import { ref, getDownloadURL, getMetadata } from "firebase/storage";
 
-// [downloadImage(url)] GETS the URL and metadata of [imagePrefix]
-// and returns the data
-// Requires: [imagePrefix] is a valid folder prefix.
-// An example of a valid folder prefix is 'images/task1'.
 export async function downloadImage(imagePrefix) {
   const imageRef = ref(storage, imagePrefix);
   var rt = {};
 
   // GET [imagePrefix] URL
-  await getDownloadURL(ref(storage, imagePrefix))
-    .then(async (url) => {
-      // Get metadata properties
-      await getMetadata(imageRef)
-        .then((fullMetadata) => {
-          rt = {
-            path: imageRef,
-            url: url,
-            metadata: fullMetadata.customMetadata,
-          };
-          console.log(rt);
-          // return {
-          //   path: imageRef,
-          //   url: url,
-          //   metadata: fullMetadata.customMetadata,
-          // };
-        })
-        .catch((error) => {
-          // Error getting metadata
-          alert(error);
-          console.log(error);
-        });
-    })
-    .catch((error) => {
-      // Error getting URL
-      alert(error);
-      console.log(error);
-    });
+  const url = await getDownloadURL(imageRef);
+  const fullMetadata = await getMetadata(imageRef);
+  rt = {
+    path: imagePrefix,
+    url: url,
+    metadata: fullMetadata.customMetadata,
+  };
   return rt;
 }
 
 // Returns: a list of image metadata by task
 export async function downloadImageByTasks(tasks, thumbnail) {
   const imageRef = collection(db, "images");
-  var images = [];
 
-  tasks.forEach(async (t) => {
+  const taskRequests = tasks.map(async (t) => {
     const q = query(imageRef, where("task", "==", t), orderBy("date", "desc"));
 
     const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach(async (doc) => {
-      var imagePath = doc.data().ref; // image/task/...
-      if (thumbnail) imagePath = imagePath.replace("images", "thumbnails");
+    const imageRequests = querySnapshot.docs.map(async (doc) => {
+      try {
+        var imagePath = doc.data().ref; // image/task/...
+        if (thumbnail) imagePath = imagePath.replace("images", "thumbnails");
 
-      // GET metadata of the image
-      images.push(await downloadImage(imagePath));
+        // GET metadata of the image
+        return await downloadImage(imagePath);
+      } catch (e) {
+        console.log(e);
+      }
     });
+    return await Promise.all(imageRequests);
   });
 
-  return images;
+  return (await Promise.all(taskRequests)).flat();
 }
 
 // Returns: a list of image metadata by user
 export async function downloadImageByUsers(users, thumbnail) {
   const imageRef = collection(db, "images");
 
-  var images = [];
-
-  users.forEach(async (email) => {
+  const usersRequests = users.map(async (email) => {
     const q = query(
       imageRef,
       where("email", "==", email),
@@ -88,25 +65,27 @@ export async function downloadImageByUsers(users, thumbnail) {
 
     const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach(async (doc) => {
+    const imageRequests = querySnapshot.docs.map(async (doc) => {
       var imagePath = doc.data().ref; // image/task/...
       if (thumbnail) imagePath = imagePath.replace("images", "thumbnails");
 
       // GET metadata of the image
-      images.push(await downloadImage(imagePath));
+      return await downloadImage(imagePath);
     });
+
+    return await Promise.all(imageRequests);
   });
 
-  return images;
+  return (await Promise.all(usersRequests)).flat();
 }
 
 // Returns: a list of image metadata by user & task
 export async function downloadImageByTasksAndUsers(tasks, users, thumbnail) {
   const imageRef = collection(db, "images");
 
-  var images = [];
+  // var images = [];
 
-  users.forEach(async (email) => {
+  const usersRequests = users.map(async (email) => {
     //each user in filter
     const q = query(
       imageRef,
@@ -116,37 +95,37 @@ export async function downloadImageByTasksAndUsers(tasks, users, thumbnail) {
 
     const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach(async (doc) => {
+    const imageRequests = querySnapshot.docs.map(async (doc) => {
       const data = doc.data();
       // if task is in filter
       if (tasks.includes(data.task)) {
         var imagePath = data.ref; // image/task/...
         if (thumbnail) imagePath = imagePath.replace("images", "thumbnails");
-
-        // GET metadata of the image
-        images.push(await downloadImage(imagePath));
+        return await downloadImage(imagePath);
       }
     });
+
+    return await Promise.all(imageRequests);
   });
 
-  return images;
+  return (await Promise.all(usersRequests)).flat();
 }
 
 export async function downloadAllImages(thumbnail) {
   const imageRef = collection(db, "images");
-  console.log("hi");
-  var images = [];
 
   const q = query(imageRef, orderBy("date", "desc"), limit(10));
   const querySnapshot = await getDocs(q);
 
-  querySnapshot.forEach(async (doc) => {
+  // Map querySnapshot to array of async functions (Promises) [forEach is synchronous!]
+  const imageRequests = querySnapshot.docs.map(async (doc) => {
     var imagePath = doc.data().ref; // image/task/...
     if (thumbnail) imagePath = imagePath.replace("images", "thumbnails");
-    console.log(imagePath);
-    // GET metadata of the image
-    images.push(await downloadImage(imagePath));
+
+    // Return image objects
+    return await downloadImage(imagePath);
   });
 
-  return images;
+  // Return array of resolved promises (i.e. the image objects)
+  return Promise.all(imageRequests);
 }
