18989e8b5447317453a2a6b237f411c0abe521f9
ruchitharajaghatta <77162985+ruchitharajaghatta@users.noreply.github.com>
2021-12-10 10:59:45 -0500

    Rsr262/bugreportsui (#18)
    * added styling to bug reports
    
    * updates to ui
    
    * changed to percentages for formatting

--- src/screens/bugReportScreen/index.js
+++ src/screens/bugReportScreen/index.js
@@ -1,12 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import { getBugReports } from '../../firebase/manage';
+import { Label } from 'semantic-ui-react';
+
+const styles = {
+  // header: {
+  //   color: 'black',
+  //   backgroundColor: 'lightBlue',
+  //   marginBottom: '2px%',
+  //   padding: '0.5%',
+
+
+  //   // marginRight: '2%',
+  // },
+  // // email: {
+  // //   color: 'black',
+  // //   textAlign: 'left',
+  // //   marginBottom: '1%',
+  // //   marginRight: '2%',
+  // //   padding: '1%'
+  // // },
+  // message: {
+  //   marginTop: '0.005%',
+  //   fontSize: '100%',
+  //   // marginBottom: '0.25%',
+  //   // marginRight: '2%',
+  //   padding: "0.01%",
+
+
+  // }
+  header: {
+    color: 'black',
+    backgroundColor: 'lightBlue',
+    marginTop: '1.5%',
+    marginRight: '0.05%',
+  },
+  email: {
+    color: 'black',
+    textAlign: 'left',
+    marginBottom: '0.5%',
+    marginRight: '0.05%',
+    padding: '0.05%'
+  },
+  message: {
+    marginBottom: '0.05%',
+    padding: "0.05%",
+  }
+};
 function BugReport({ email, message, time }) {
   return (
     <body>
-      <h1>Bug Report {time}</h1>
-      <h2>Reported By: {email}</h2>
-      <p>Message: {message}</p>
+      <section class="index align-left">
+        <p style={styles.email}><Label style={styles.header}>Bug Reported By:&nbsp;&nbsp;&nbsp;{email}</Label></p>
+
+        <p style={styles.message}><p style={{ fontWeight: 'bold' }}>Message:&nbsp;&nbsp;</p>{message}</p>
+      </section>
     </body>
+
+
   );
 }
 
@@ -20,7 +70,7 @@ export const BugReportScreen = () => {
 
   function getReports() {
     const componentList = bugList.map((item) => (
-      <BugReport email={item.user} message={item.description} />
+      <BugReport email={item.user} message={item.description} time={item.date} />
     ));
     return componentList;
   }
