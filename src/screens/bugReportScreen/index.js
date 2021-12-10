75232304fd9813d57b00252702af6dd14d45a606
Ruchitha Rajaghatta <ruchitha.sr@gmail.com>
2021-12-10 10:31:55 -0500

    changed to percentages for formatting

--- src/screens/bugReportScreen/index.js
+++ src/screens/bugReportScreen/index.js
@@ -3,36 +3,56 @@ import { getBugReports } from '../../firebase/manage';
 import { Label } from 'semantic-ui-react';
 
 const styles = {
+  // header: {
+  //   color: 'black',
+  //   backgroundColor: 'lightBlue',
+  //   marginBottom: '2px%',
+  //   padding: '0.5%',
+
+
+  //   // marginRight: '2%',
+  // },
+  // // email: {
+  // //   color: 'black',
+  // //   textAlign: 'left',
+  // //   marginBottom: '1%',
+  // //   marginRight: '2%',
+  // //   padding: '1%'
+  // // },
+  // message: {
+  //   marginTop: '0.005%',
+  //   fontSize: '100%',
+  //   // marginBottom: '0.25%',
+  //   // marginRight: '2%',
+  //   padding: "0.01%",
+
+
+  // }
   header: {
     color: 'black',
     backgroundColor: 'lightBlue',
-    marginBottom: '5px',
-    marginTop: '20px',
-    marginRight: '5px',
+    marginTop: '1.5%',
+    marginRight: '0.05%',
   },
   email: {
     color: 'black',
     textAlign: 'left',
-    marginBottom: '2px',
-    marginRight: '5px',
-
-    padding: '2px'
+    marginBottom: '0.5%',
+    marginRight: '0.05%',
+    padding: '0.05%'
   },
   message: {
-    marginBottom: '5px',
-    marginRight: '5px',
-    padding: "5px",
-
-
+    marginBottom: '0.05%',
+    padding: "0.05%",
   }
 };
 function BugReport({ email, message, time }) {
   return (
     <body>
       <section class="index align-left">
-        <p style={styles.email}><Label style={styles.header}>Bug Reported By:   {email}</Label></p>
+        <p style={styles.email}><Label style={styles.header}>Bug Reported By:&nbsp;&nbsp;&nbsp;{email}</Label></p>
 
-        <p style={styles.message}>Message: {message}</p>
+        <p style={styles.message}><p style={{ fontWeight: 'bold' }}>Message:&nbsp;&nbsp;</p>{message}</p>
       </section>
     </body>
 
